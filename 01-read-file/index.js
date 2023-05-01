8c873176352c55ef2759b4cbb0c00828087b29b3
Vladislav Svirydovich <vladsvir888@gmail.com>
2023-05-01 16:34:13 +0300

    feat: add 01 read file

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+readStream.on('data', (chunk) => console.log(chunk));
