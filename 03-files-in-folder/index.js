dee4b861b0cc65aa45953055b716aa833cb364c2
Vladislav Svirydovich <vladsvir888@gmail.com>
2023-05-02 21:51:50 +0300

    feat: add 03 files in folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true }, (err, items) => {
+    items.forEach(item => {
+        if (!item.isDirectory()) {
+            fs.stat(path.join(__dirname, 'secret-folder', `${item.name}`), (err, stats) => {
+                console.log(`${item.name.split('.').join(' - ')} - ${stats.size} bytes`);
+            });
+        }
+    });
+});
