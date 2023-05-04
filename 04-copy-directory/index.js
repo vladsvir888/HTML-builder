b266d0a12a14ee70be15956de1886701836efbed
Vladislav Svirydovich <vladsvir888@gmail.com>
2023-05-05 02:33:37 +0300

    feat: add 04 copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const { mkdir, readdir, copyFile, rm } = require('fs/promises');
+const path = require('path');
 
+const pathFiles = path.join(__dirname, 'files');
+const pathCopyFiles = path.join(__dirname, 'files-copy');
+
+async function copyDir() {
+    try {
+        await rm(pathCopyFiles, { recursive: true, force: true });
+
+        await mkdir(pathCopyFiles, { recursive: true });
+
+        const files = await readdir(pathFiles);
+
+        files.forEach(file => {
+            copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+copyDir();
